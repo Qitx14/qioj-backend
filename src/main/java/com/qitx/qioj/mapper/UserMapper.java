429a2127d1f6ff8f2e28e3ce6ee47c23ab45a0e4
Qitx14 <2197787573@qq.com>
2024-01-23 23:45:35 +0800

    Project initialization

---
+++ src/main/java/com/qitx/qioj/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.qitx.qioj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qitx.qioj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
